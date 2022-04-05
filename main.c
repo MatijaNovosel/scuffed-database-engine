637d7d0e9e48997108b869a66e1e0d3fdb6ec94a
Matija Novosel <mnovosel5@gmail.com>
2022-04-05 16:19:52 +0200

    DB creation wip

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <memory.h>
+#include <stdlib.h>
 
-#define dbListFileName "dbList.txt"
+#define dbListFileName "dbList.dat"
+#define bufferLimit 255
 
 int fileExists(char *fileName)
 {
   FILE *file;
-  if ((file = fopen(fileName, "r")) != NULL)
+  if ((file = fopen(fileName, "rb")) != NULL)
   {
     fclose(file);
     return 1;
@@ -17,12 +18,20 @@ int fileExists(char *fileName)
   }
 }
 
+typedef struct DatabaseDefinition
+{
+  char *name;
+  char **tableNames;
+  int tableCount;
+} DatabaseDefinition;
+
 typedef struct DBEngine
 {
   void (*createDb)(DBEngine *self, char *dbName);
   void (*createTable)(DBEngine *self, char *dbName, char *tableName);
   int (*isDbListEmpty)();
   void (*createDbList)();
+  int (*dbExists)(char *dbName);
 } DBEngine;
 
 void createDb(DBEngine *self, char *dbName)
@@ -31,10 +40,26 @@ void createDb(DBEngine *self, char *dbName)
   {
     self->createDbList();
   }
+
+  FILE *fp = fopen(dbListFileName, "rb");
+
+  if (!(self->dbExists(dbName)))
+  {
+    DatabaseDefinition databaseDefinition;
+    fread(&databaseDefinition, sizeof(DatabaseDefinition), 1, fp);
+    printf("%s", databaseDefinition.name);
+  }
   else
   {
-    //
+    DatabaseDefinition databaseDefinition;
+    databaseDefinition.name = dbName;
+    databaseDefinition.tableCount = 1;
+    databaseDefinition.tableNames = malloc(sizeof(char *));
+    databaseDefinition.tableNames[0] = malloc(5 * sizeof(char));
+    fwrite(&databaseDefinition, sizeof(databaseDefinition), 1, fp);
   }
+
+  fclose(fp);
 }
 
 void createTable(DBEngine *self, char *dbName, char *tableName)
@@ -46,8 +71,13 @@ void createTable(DBEngine *self, char *dbName, char *tableName)
 
 void createDbList()
 {
-  FILE *fp;
-  fp = fopen(dbListFileName, "w");
+  FILE *fp = fopen(dbListFileName, "wb");
+  fclose(fp);
+}
+
+int dbExists(char *dbName)
+{
+  FILE *fp = fopen(dbListFileName, "wb");
   fclose(fp);
 }
 
@@ -69,6 +99,8 @@ DBEngine ConstructDBEngine()
   dbEngine.createDb = &createDb;
   dbEngine.isDbListEmpty = &isDbListEmpty;
   dbEngine.createTable = &createTable;
+  dbEngine.createDbList = &createDbList;
+  dbEngine.dbExists = &dbExists;
   return dbEngine;
 }
 
@@ -76,5 +108,4 @@ void main()
 {
   DBEngine db = ConstructDBEngine();
   db.createDb(&db, "mainDb");
-  db.isDbListEmpty();
 }

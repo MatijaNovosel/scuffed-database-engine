7e0850c41df152c20b8409dbe6c42f528765b5bd
Matija Novosel <mnovosel5@gmail.com>
2022-04-06 10:36:26 +0200

    DB list reading

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define dbListFileName "dbList.dat"
 #define bufferLimit 255
@@ -27,11 +28,14 @@ typedef struct DatabaseDefinition
 
 typedef struct DBEngine
 {
+  FILE *dbList;
   void (*createDb)(void *self, char *dbName);
   void (*createTable)(void *self, char *dbName, char *tableName);
   int (*isDbListEmpty)();
   void (*createDbList)();
-  int (*dbExists)(char *dbName);
+  int (*dbExists)(void *self, char *dbName);
+  void (*openConnection)(void *self);
+  void (*closeConnection)(void *self);
 } DBEngine;
 
 void createDb(void *self, char *dbName)
@@ -43,25 +47,22 @@ void createDb(void *self, char *dbName)
     this->createDbList();
   }
 
-  FILE *fp = fopen(dbListFileName, "rb");
-
-  if (!(this->dbExists(dbName)))
+  if (this->dbExists(self, dbName))
   {
     DatabaseDefinition databaseDefinition;
-    fread(&databaseDefinition, sizeof(DatabaseDefinition), 1, fp);
+    fread(&databaseDefinition, sizeof(DatabaseDefinition), 1, this->dbList);
     printf("%s", databaseDefinition.name);
   }
   else
   {
     DatabaseDefinition databaseDefinition;
-    databaseDefinition.name = dbName;
+    databaseDefinition.name = malloc(sizeof(char) * strlen(dbName));
+    strcpy(databaseDefinition.name, dbName);
     databaseDefinition.tableCount = 1;
     databaseDefinition.tableNames = malloc(sizeof(char *));
     databaseDefinition.tableNames[0] = malloc(5 * sizeof(char));
-    fwrite(&databaseDefinition, sizeof(databaseDefinition), 1, fp);
+    fwrite(&databaseDefinition, sizeof(databaseDefinition), 1, this->dbList);
   }
-
-  fclose(fp);
 }
 
 void createTable(void *self, char *dbName, char *tableName)
@@ -77,22 +78,38 @@ void createDbList()
   fclose(fp);
 }
 
-int dbExists(char *dbName)
+int dbExists(void *self, char *dbName)
 {
-  FILE *fp = fopen(dbListFileName, "wb");
-  fclose(fp);
+  DBEngine *this = (DBEngine *)self;
+  return 1;
 }
 
 int isDbListEmpty()
 {
   if (fileExists(dbListFileName))
   {
-    printf("File exists!");
+    return 1;
   }
   else
   {
     return 0;
   }
+}
+
+void openConnection(void *self)
+{
+  DBEngine *this = (DBEngine *)self;
+  if (this->isDbListEmpty())
+  {
+    this->createDbList();
+  }
+  this->dbList = fopen(dbListFileName, "rb");
+}
+
+void closeConnection(void *self)
+{
+  DBEngine *this = (DBEngine *)self;
+  fclose(this->dbList);
 }
 
 DBEngine ConstructDBEngine()
@@ -103,11 +120,15 @@ DBEngine ConstructDBEngine()
   dbEngine.createTable = &createTable;
   dbEngine.createDbList = &createDbList;
   dbEngine.dbExists = &dbExists;
+  dbEngine.openConnection = &openConnection;
+  dbEngine.closeConnection = &closeConnection;
   return dbEngine;
 }
 
 void main()
 {
   DBEngine db = ConstructDBEngine();
+  db.openConnection(&db);
   db.createDb(&db, "mainDb");
+  db.closeConnection(&db);
 }

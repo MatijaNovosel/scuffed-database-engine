a1a00453dcbf8d22daae115e2d66bfbcd2883d5b
Matija Novosel <mnovosel5@gmail.com>
2022-04-06 11:54:15 +0200

    DBlist reading

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #define dbListFileName "dbList.dat"
 #define bufferLimit 255
+#define dbNameCharLimit 255
 
 int fileExists(char *fileName)
 {
@@ -21,8 +22,7 @@ int fileExists(char *fileName)
 
 typedef struct
 {
-  char *name;
-  char **tableNames;
+  char name[dbNameCharLimit];
   int tableCount;
 } DatabaseDefinition;
 
@@ -49,19 +49,17 @@ void createDb(void *self, char *dbName)
   }
   else
   {
-    DatabaseDefinition databaseDefinition;
+    DatabaseDefinition *databaseDefinition = malloc(sizeof(DatabaseDefinition));
+    databaseDefinition->tableCount = 1;
 
-    databaseDefinition.name = malloc(sizeof(char) * (strlen(dbName) + 1));
-    strcpy(databaseDefinition.name, dbName);
+    strcpy(databaseDefinition->name, dbName);
+    printf("%s\n", databaseDefinition->name);
 
-    printf("%s\n", databaseDefinition.name);
-
-    databaseDefinition.tableCount = 1;
-    databaseDefinition.tableNames = malloc(sizeof(char *));
-    databaseDefinition.tableNames[0] = malloc(5 * sizeof(char));
-    fwrite(&databaseDefinition, sizeof(databaseDefinition), 1, this->dbList);
+    fwrite(databaseDefinition, sizeof(databaseDefinition), 1, this->dbList);
 
     DatabaseDefinition dd;
+
+    rewind(this->dbList);
     fread(&dd, sizeof(DatabaseDefinition), 1, this->dbList);
     printf("%s", dd.name);
   }
@@ -76,7 +74,7 @@ void createTable(void *self, char *dbName, char *tableName)
 
 void createDbList()
 {
-  FILE *fp = fopen(dbListFileName, "wb");
+  FILE *fp = fopen(dbListFileName, "wb+");
   fclose(fp);
 }
 
@@ -93,7 +91,7 @@ void openConnection(void *self)
   {
     this->createDbList();
   }
-  this->dbList = fopen(dbListFileName, "rb");
+  this->dbList = fopen(dbListFileName, "wb+");
 }
 
 void closeConnection(void *self)
@@ -102,22 +100,22 @@ void closeConnection(void *self)
   fclose(this->dbList);
 }
 
-DBEngine ConstructDBEngine()
+DBEngine *ConstructDBEngine()
 {
-  DBEngine dbEngine;
-  dbEngine.createDb = &createDb;
-  dbEngine.createTable = &createTable;
-  dbEngine.createDbList = &createDbList;
-  dbEngine.dbExists = &dbExists;
-  dbEngine.openConnection = &openConnection;
-  dbEngine.closeConnection = &closeConnection;
+  DBEngine *dbEngine = malloc(sizeof(DBEngine));
+  dbEngine->createDb = &createDb;
+  dbEngine->createTable = &createTable;
+  dbEngine->createDbList = &createDbList;
+  dbEngine->dbExists = &dbExists;
+  dbEngine->openConnection = &openConnection;
+  dbEngine->closeConnection = &closeConnection;
   return dbEngine;
 }
 
 void main()
 {
-  DBEngine db = ConstructDBEngine();
-  db.openConnection(&db);
-  db.createDb(&db, "mainDb");
-  db.closeConnection(&db);
+  DBEngine *db = ConstructDBEngine();
+  db->openConnection(db);
+  db->createDb(db, "mainDb");
+  db->closeConnection(db);
 }
